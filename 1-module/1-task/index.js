2660bdb02219fdce8c6ab46e765fb56214b10652
Vyacheslav Leshkevich <klenwork17@gmail.com>
2024-04-29 07:41:02 +0300

    1-module 1 task completed

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
-function factorial(n) {
-  // ваш код...
+function factorial(n){
+  let result = n;
+    
+  if(result === 1 || result === 0){
+        return 1;
+  }
+
+  for (let i = n - 1; i > 1; i--){
+    result = result * i; 
+  }
+
+  return result;
 }
+
