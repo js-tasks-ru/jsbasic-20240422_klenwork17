59a33b001c925a547da816e4ce03c3307e2bf017
Vyacheslav Leshkevich <klenwork17@gmail.com>
2024-05-06 19:20:21 +0300

    finished 3-module-3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,19 @@
-function camelize(str) {
-  // ваш код...
+// Напишите функцию camelize(str),
+// которая преобразует строки вида 'my-short-string' в 'myShortString'.
+
+// То есть дефисы удаляются, а все слова после них получают заглавную букву.
+// Примеры:
+
+camelize('background-color') == 'backgroundColor';
+
+function camelize(str){
+  str = str.split("-");
+  str = str.map((word, index) => (index == 0) ? word : word[0].toUpperCase() + word.slice(1));
+  str = str.join("");
+
+  return str;
 }
+
+// P.S. Подсказка: используйте метод split,
+// чтобы разбить строку на массив символов,
+// потом переделайте всё как нужно и методом join соедините обратно.
