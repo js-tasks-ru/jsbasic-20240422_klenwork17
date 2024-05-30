57c18813d536199918fd500022494f4559544e30
Vyacheslav Leshkevich <klenwork17@gmail.com>
2024-05-30 20:36:50 +0300

    done

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,31 @@
-function makeFriendsList(friends) {
-  // ваш код...
+let friends = [
+  {
+      firstName: 'Artsiom',
+      lastName: 'Mezin'
+  },
+  {
+      firstName: 'Ilia',
+      lastName: 'Kantor'
+  },
+  {
+      firstName: 'Christopher',
+      lastName: 'Michael'
+  }
+];
+
+
+
+function makeFriendsList(obj){
+  let ul = document.createElement('ul');
+  document.body.append(ul);
+
+  let names = obj.map(el => el.firstName + ' ' + el.lastName)
+  
+  for(let i = 0; i < names.length; i++){
+    let li = document.createElement('li');
+    li.innerHTML = names[i];
+    ul.append(li);
+  }
+
+  return ul;
 }
