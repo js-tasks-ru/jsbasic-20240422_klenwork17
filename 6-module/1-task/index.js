9750a1064901d89fce0c4b4e3c7df25d6ae65968
Vyacheslav Leshkevich <klenwork17@gmail.com>
2024-07-15 19:50:13 +0300

    wrong

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,79 @@
  *
  */
 export default class UserTable {
-  constructor(rows) {
+  constructor(rows){
+      this.rows = rows;
+      this.elem = this.table();
+  }
+
+  table() {
+      const table = document.createElement('table');
+      const BODY = document.body
+
+      BODY.append(table);
+      
+      // Создание шапки таблицы
+
+      const thead = document.createElement('thead');
+      table.append(thead)
+
+      const headerRow = document.createElement('tr'); 
+      thead.append(headerRow)
+
+      Object.keys(this.rows[0]).forEach(function(item){
+          const th = document.createElement('th');
+          th.textContent = item;
+
+          headerRow.append(th)
+
+      })
+      const th = document.createElement('th');
+      headerRow.append(th)
+
+
+      // Создание Тела таблицы
+
+      const tbody = document.createElement('tbody');
+      table.append(tbody);
+
+      // Для каждого объекта массива создаём строку, заполняем её количеством ячеек, 
+      // которое равно количеству свойств. Содержимое ячеек - значение свойства
+
+      this.rows.forEach(function(item){
+          const tr = document.createElement('tr');
+
+          for(let key in item){
+              const td = document.createElement('td');
+              td.textContent = item[key];
+
+              tr.append(td);
+          }
+
+          const tdButtonContainer = document.createElement('td'); // ячейка для кнпоки
+          tr.append(tdButtonContainer)
+
+          const deleteButton = document.createElement('button');    
+          deleteButton.className = 'btn';
+          deleteButton.textContent = '[x]';   
+
+          tdButtonContainer.append(deleteButton);
+
+          // Обработчики
+
+          deleteButton.addEventListener('click', function(event){
+              deleteButton.closest('tr').remove()   
+          })  
+          
+          tbody.append(tr);
+          
+      })
   }
 }
+
+
+
+
+
+
+
+
