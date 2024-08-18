4df7f5c9c46736cf1cb2576d8fb1ae8dfee3e5c2
Vyacheslav Leshkevich <klenwork17@gmail.com>
2024-08-18 13:56:11 +0300

    7-3 has not done

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,3 +1,5 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class Modal {
   constructor() {
     this.render();
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,7 @@
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    
   }
+  
 }
