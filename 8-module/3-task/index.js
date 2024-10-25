b87501c94d14aca9305aa8d9a413bc5febcd2e0b
Vyacheslav Leshkevich <klenwork17@gmail.com>
2024-10-25 14:22:30 +0300

    cart done

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,29 +6,56 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product) return;
+
+    let newProduct = {
+      product: product,
+      count: 1
+    }
+
+    let existingProduct = this.cartItems.find((cartItem) => cartItem.product.id === newProduct.product.id);
+
+    if (!existingProduct) {
+      this.cartItems.push(newProduct);
+    } else {
+      existingProduct.count++;
+    }
+
+    this.onProductUpdate(this.cartItems); // обновление корзины
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let currentProduct = this.cartItems.find((cartItem) => cartItem.product.id === productId);
+
+    if (amount === 1) currentProduct.count++;  
+    else if (amount === -1) currentProduct.count--;
+
+    if (currentProduct.count === 0) {
+      let emptyProductIndex =  this.cartItems.indexOf(currentProduct);
+      this.cartItems.splice(emptyProductIndex, 1);
+    }
+
+    this.onProductUpdate(this.cartItems);
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;
   }
 
-  getTotalCount() {
-    // ваш код
+  getTotalCount() {   
+    let count = this.cartItems.reduce((sum, cartItem) => sum + cartItem.count, 0);
+    return count;
   }
 
   getTotalPrice() {
-    // ваш код
+    let sum = this.cartItems.reduce((currentSum, cartItem) => currentSum + (cartItem.product.price * cartItem.count), 0);
+    return sum;
   }
 
   onProductUpdate(cartItem) {
-    // реализуем в следующей задаче
 
-    this.cartIcon.update(this);
+    this.cartIcon.update(this); // обновление корзины
   }
 }
+
 
